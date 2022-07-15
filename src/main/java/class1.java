74906ca4eeabdfb8922dd903d35c9444bbb0e015
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-07-15 10:11:26 +0400

    minimum arr element finder

---
+++ src/main/java/class1.java
@@ -0,0 +1,20 @@
+public class class1 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[0];
+        System.out.println(min(arr1));
+    }
+
+    public static int min(int[] arr) {
+
+        int min = arr[0];
+
+        for(var num: arr) {
+            if (num<min) {
+                min = num;
+            }
+        }
+
+        return min;
+    }
+    
+}
