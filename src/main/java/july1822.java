20652c7072d4faef8b2fbbc8951d4695cbd7cff8
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-07-18 19:34:47 +0400

    also falled when equal to s or t added

--- src/main/java/july1822.java
+++ src/main/java/july1822.java
@@ -2,48 +2,28 @@ import java.util.Scanner;
 
 public class july1822 {
     public static void main(String[] args) {
-        Scanner sn = new Scanner(System.in);
-        int s = Integer.parseInt(sn.nextLine().split(" ")[0]);
-        int t = Integer.parseInt(sn.nextLine().split("")[1]);
-        int a = Integer.parseInt(sn.nextLine().split("")[0]);
-        int b = Integer.parseInt(sn.nextLine().split("")[1]);
-        int m = Integer.parseInt(sn.nextLine().split("")[0]);
-        int n = Integer.parseInt(sn.nextLine().split("")[1]);
-
-        //apples distances from a
-        String[] applesDistStrArr = sn.nextLine().split(" ");
-        int[] applesDist = new int[m];
-
-        for (int i = 0; i < applesDist.length; i++) {
-            applesDist[i] = Integer.parseInt(applesDistStrArr[i]);
-        }
-
-        //orange distances from b
-        String[] orangesDistStrArr = sn.nextLine().split(" ");
-        int[] orangesDist = new int[m];
-
-        for (int i = 0; i < orangesDist.length; i++) {
-            orangesDist[i] = Integer.parseInt(orangesDistStrArr[i]);
-        }
-
-        countApplesAndOranges(s, t, a, b, applesDist, orangesDist);
+        countApplesAndOranges(7 ,11,
+                5 ,15,
+                        new int[]{-2, 2, 1}, new int[]{5, -6});
 
     }
 
     public static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
         int countApples = 0;
         int countOranges = 0;
+        int locApple;
+        int locOrange;
 
         for (int i = 0; i < apples.length; i++) {
-            int locApple = apples[i] + a;
-            if(locApple>s && locApple<t) {
+            locApple = apples[i] + a;
+            if(locApple>=s && locApple<=t) {
                 countApples++;
             }
         }
 
         for (int i = 0; i < oranges.length; i++) {
-            int locOrange = oranges[i] + b;
-            if(locOrange>s && locOrange<t) {
+            locOrange = oranges[i] + b;
+            if(locOrange>=s && locOrange<=t) {
                 countOranges++;
             }
         }
