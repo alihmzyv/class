bac6d27e0c38ea20d3a7fdf364db8334b06de16d
alihmzyv <92953261+alihmzyv@users.noreply.github.com>
2022-08-21 10:27:02 +0400

    only streams

--- src/main/java/august2122/PossibleMoves.java
+++ src/main/java/august2122/PossibleMoves.java
@@ -4,28 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PossibleMoves {
     public static void main(String[] args) {
         System.out.println(possibleMoves("e4"));
     }
     public static List<String> possibleMoves(String pos) {
-        List<String> possibleMoves = new ArrayList<>();
         char letter = pos.charAt(0);
         int digit = Integer.parseInt(String.valueOf(pos.charAt(1)));
-        List<String> possibleMoves1 = IntStream.rangeClosed(1, 8)
+        Stream<String> possibleMoves1 = IntStream.rangeClosed(1, 8)
                 .filter(i -> i != digit)
                 .boxed()
-                .map(i -> String.format("%s%d", letter, i))
-                .collect(Collectors.toList());
+                .map(i -> String.format("%s%d", letter, i));
 
-        List<String> possibleMoves2 = IntStream.rangeClosed(97, 104)
+        Stream<String> possibleMoves2 = IntStream.rangeClosed(97, 104)
                 .mapToObj(i -> (char) i)
                 .filter(i -> i != letter)
-                .map(i -> String.format("%s%d", i, digit))
-                .collect(Collectors.toList());
-        possibleMoves.addAll(possibleMoves1);
-        possibleMoves.addAll(possibleMoves2);
-        return possibleMoves;
+                .map(i -> String.format("%s%d", i, digit));
+        return Stream.concat(possibleMoves1, possibleMoves2).toList();
     }
 }
