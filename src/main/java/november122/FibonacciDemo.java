2e75ab4d4ba41f9069aecbce867b2e2e9ca56665
alihmzyv <alihmzyv@gmail.com>
2022-11-01 19:38:59 +0400

    updated

--- src/main/java/november122/FibonacciDemo.java
+++ src/main/java/november122/FibonacciDemo.java
@@ -6,25 +6,23 @@ import java.util.stream.IntStream;
 
 public class FibonacciDemo {
     public static List<Integer> getFibonacciSequence(int firstN) {
-        List<Integer> sequence = new ArrayList<>();
         if (firstN <= 0) {
             throw new IllegalArgumentException("Number of elements should be greater than 0");
         }
-        if (firstN == 1) {
-            sequence.add(0);
-            return sequence;
-        }
-        if (firstN == 2) {
-            sequence.addAll(List.of(0, 1));
-            return sequence;
-        }
-        sequence.addAll(List.of(0, 1));
-        IntStream.range(2, firstN)
-                .forEach(i -> sequence.add(sequence.get(i - 1) + sequence.get(i - 2)));
+        List<Integer> sequence = new ArrayList<>(firstN);
+        IntStream.range(0, firstN)
+                .forEach(i -> {
+                    if (i == 0 || i == 1) {
+                        sequence.add(i);
+                    }
+                    else {
+                        sequence.add(sequence.get(i - 1) + sequence.get(i - 2));
+                    }
+                });
         return sequence;
     }
 
     public static void main(String[] args) {
-        System.out.println(getFibonacciSequence(2));
+        System.out.println(getFibonacciSequence(5));
     }
 }
